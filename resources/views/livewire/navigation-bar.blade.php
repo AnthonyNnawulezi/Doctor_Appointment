a54cba9c3dc82694f0deef2fdccb0428c7285f25
AntonsPrime <evansanton58@gmail.com>
2026-08-01 01:31:19 +0100

    components for navigation bar

---
+++ resources/views/livewire/navigation-bar.blade.php
@@ -0,0 +1,44 @@
+<div class="space-y-4">
+    <header class="flex flex-wrap sm:justify-start sm:flex-nowrap w-full py-3 bg-primary">
+        <nav class="max-w-340 w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
+            <div class="flex items-center justify-between">
+                <a class="flex-none text-xl font-semibold text-foreground-inverse focus:outline-hidden focus:opacity-80"
+                    href="#">Brand</a>
+                <div class="sm:hidden">
+                    <button type="button"
+                        class="hs-collapse-toggle relative size-9 flex justify-center items-center gap-2 rounded-lg border border-line-inverse/30 text-foreground-inverse shadow-2xs hover:bg-plain/20 focus:outline-hidden focus:bg-plain/20"
+                        id="hs-navbar-primary-collapse" aria-expanded="false" aria-controls="hs-navbar-primary"
+                        aria-label="Toggle navigation" data-hs-collapse="#hs-navbar-primary">
+                        <svg class="hs-collapse-open:hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg"
+                            width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <line x1="3" x2="21" y1="6" y2="6" />
+                            <line x1="3" x2="21" y1="12" y2="12" />
+                            <line x1="3" x2="21" y1="18" y2="18" />
+                        </svg>
+                        <svg class="hs-collapse-open:block hidden shrink-0 size-4" xmlns="http://www.w3.org/2000/svg"
+                            width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M18 6 6 18" />
+                            <path d="m6 6 12 12" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div id="hs-navbar-primary"
+                class="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow sm:block"
+                aria-labelledby="hs-navbar-primary-collapse" role="region">
+                <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+                    <a href="#"
+                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                        Log in
+                    </a>
+                    <a href="#"
+                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
+                        Register
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,59 +20,12 @@
         <div
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
-                <div class="space-y-4">
-                    <header class="flex flex-wrap sm:justify-start sm:flex-nowrap w-full py-3 bg-primary">
-                        <nav class="max-w-340 w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
-                            <div class="flex items-center justify-between">
-                                <a class="flex-none text-xl font-semibold text-foreground-inverse focus:outline-hidden focus:opacity-80"
-                                    href="#">Brand</a>
-                                <div class="sm:hidden">
-                                    <button type="button"
-                                        class="hs-collapse-toggle relative size-9 flex justify-center items-center gap-2 rounded-lg border border-line-inverse/30 text-foreground-inverse shadow-2xs hover:bg-plain/20 focus:outline-hidden focus:bg-plain/20"
-                                        id="hs-navbar-primary-collapse" aria-expanded="false"
-                                        aria-controls="hs-navbar-primary" aria-label="Toggle navigation"
-                                        data-hs-collapse="#hs-navbar-primary">
-                                        <svg class="hs-collapse-open:hidden shrink-0 size-4"
-                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round">
-                                            <line x1="3" x2="21" y1="6" y2="6" />
-                                            <line x1="3" x2="21" y1="12" y2="12" />
-                                            <line x1="3" x2="21" y1="18" y2="18" />
-                                        </svg>
-                                        <svg class="hs-collapse-open:block hidden shrink-0 size-4"
-                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round">
-                                            <path d="M18 6 6 18" />
-                                            <path d="m6 6 12 12" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                            <div id="hs-navbar-primary"
-                                class="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow sm:block"
-                                aria-labelledby="hs-navbar-primary-collapse" role="region">
-                                <div
-                                    class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
-                                    <a class="text-sm font-medium text-foreground-inverse focus:outline-hidden"
-                                        href="#" aria-current="page">Landing</a>
-                                    <a class="text-sm text-foreground-inverse/70 hover:text-foreground-inverse focus:outline-hidden focus:text-foreground-inverse"
-                                        href="#">Account</a>
-                                    <a class="text-sm text-foreground-inverse/70 hover:text-foreground-inverse focus:outline-hidden focus:text-foreground-inverse"
-                                        href="#">Work</a>
-                                    <a class="text-sm text-foreground-inverse/70 hover:text-foreground-inverse focus:outline-hidden focus:text-foreground-inverse"
-                                        href="#">Blog</a>
-                                </div>
-                            </div>
-                        </nav>
-                    </header>
-                </div>
+                <livewire:navigation-bar />
 
                 <main class="mt-6">
                     <!-- Hero -->
-                    <livewire:herosection>
-                        <!-- End Hero -->
+                    <livewire:herosection />
+                    <!-- End Hero -->
                 </main>
 
 
