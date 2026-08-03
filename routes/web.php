d7467b01159304fe5f2fdd655dea6caee01059d1
AntonsPrime <evansanton58@gmail.com>
2026-08-03 21:47:33 +0100

    grouped middleware for doctor

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DoctorController;
+use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -14,7 +15,10 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
-Route::get('doctor-dashboard', [DoctorController::class, 'loadDoctorDashboard'])->middleware('doctor')->name('doctor.dashboard');
+Route::group(['middleware' => "doctor"], function () {
+    Route::get('doctor-dashboard', [DoctorController::class, 'loadDoctorDashboard'])->name('doctor.dashboard');
+    Route::get('recent-appointments', [DoctorController::class, 'loadDoctorDashboard'])->name('doctor.recent-appointments');
+});
 
 Route::get('admin-dashboard', [AdminController::class, 'loadAdminDashboard'])->middleware('admin')->name('admin.dashboard');
 
