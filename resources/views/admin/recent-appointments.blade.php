8885ed2d2db1aa1eeda758fa1688505a770fb906
AntonsPrime <evansanton58@gmail.com>
2026-08-04 01:24:14 +0100

    added links to navigations

--- resources/views/admin/recent-appointments.blade.php
+++ resources/views/admin/recent-appointments.blade.php
@@ -1,3 +1,16 @@
 <x-app-layout>
-    <livewire:recent-appointments>
+    <x-slot name="header">
+        <livewire:layout.navigation />
+    </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <livewire:recent-appointments>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <livewire:layout.navigation />
+        <livewire:navigation-bar />
         <h2 class="text-xl font-semibold leading-tight text-gray-800">
             {{ __('Patient Dashboard') }}
         </h2>
