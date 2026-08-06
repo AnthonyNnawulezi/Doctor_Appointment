d74bd848110770b5ef0378ffbb0aa6a5c29316bd
AntonsPrime <evansanton58@gmail.com>
2026-08-06 18:06:50 +0100

    View [admin.doctor-listings] not found. fix on folder level

--- app/Livewire/SpecialityForm.php
+++ app/Livewire/SpecialityForm.php
@@ -6,6 +6,11 @@ use Livewire\Component;
 
 class SpecialityForm extends Component
 {
+    public function register()
+    {
+        // return view('livewire.speciality-form');
+    }
+
     public function render()
     {
         return view('livewire.speciality-form');
--- resources/views/livewire/speciality-form.blade.php
+++ resources/views/livewire/speciality-form.blade.php
@@ -1,3 +1,22 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+    <form wire:submit="register">
+        <!-- Name -->
+        <div>
+            <x-input-label for="speciality" :value="__('Speciality')" />
+            <x-text-input wire:model="speciality" id="speciality" class="block mt-1 w-full" type="text"
+                speciality="speciality" required autofocus autocomplete="speciality" />
+            <x-input-error :messages="$errors->get('speciality')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a href="/admin/doctors" type="button"
+                class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                Cancel
+            </a>
+
+            <x-primary-button class="ms-4">
+                {{ __('Register') }}
+            </x-primary-button>
+        </div>
+    </form>
 </div>
