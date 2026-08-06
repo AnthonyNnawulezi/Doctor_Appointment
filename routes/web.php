84789dd82fb419adefd054086d6340ad31d442aa
AntonsPrime <evansanton58@gmail.com>
2026-08-06 16:57:21 +0100

    speciality route

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ Route::group(['middleware' => 'admin'], function () {
     Route::get('admin-dashboard', [AdminController::class, 'loadAdminDashboard'])->name('admin.dashboard');
     Route::get('/admin/doctors', [AdminController::class, 'loadAppointments'])->name('admin.doctor-listings');
     Route::get('/admin/doctor/create', [AdminController::class, 'doctorCreate'])->name('admin.create-doctor');
+    Route::get('/admin/specialities', [AdminController::class, 'loadSpecialities'])->name('admin.specialites');
 });
 
 require __DIR__ . '/auth.php';
