037f8b73bce7d9a765d6c9c57649739ed777a6ff
AntonsPrime <evansanton58@gmail.com>
2026-08-07 23:02:13 +0100

    mounted specialities

--- app/Livewire/Specialities.php
+++ app/Livewire/Specialities.php
@@ -6,9 +6,15 @@ use Livewire\Component;
 
 class Specialities extends Component
 {
+    public $specialities = [];
+
+    public function mount()
+    {
+        $this->specialities = Specialities::class;
+    }
 
     public function render()
     {
-        return view('livewire.specialities');
+        return view('livewire.specialities', ['specialities' => $this->specialities]);
     }
 }
