a1876998d5a822da7503edf0dd60281d4b2ad88c
AntonsPrime <evansanton58@gmail.com>
2026-08-09 22:48:32 +0100

    policy for extra secutity

---
+++ app/Policies/SpecialityPolicy.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use App\Models\Speciality;
+
+class SpecialityPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    public function delete(User $user, Speciality $speciality): bool
+    {
+        return $user->role === 'admin';
+    }
+}
