75e40ff96f5a87de7643885249ff50b716b753c1
AntonsPrime <evansanton58@gmail.com>
2026-08-11 22:34:44 +0100

    success and error message seperation of concerns

--- resources/views/livewire/specialities.blade.php
+++ resources/views/livewire/specialities.blade.php
@@ -5,7 +5,8 @@
             aria-labelledby="hs-solid-color-success-label">
             <span id="hs-solid-color-success-label" class="font-bold">Success</span> {{ session('success') }}
         </div>
-    @else
+    @endif
+    @if (session()->has('error'))
         <div class="bg-red-500 text-sm text-foreground-inverse rounded-lg p-4" role="alert" tabindex="-1"
             aria-labelledby="hs-solid-color-success-label">
             <span id="hs-solid-color-success-label" class="font-bold">Error!</span> {{ session('error') }}
